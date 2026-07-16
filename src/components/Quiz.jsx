4d0a4febdc15a8e68a3cb667cc1926b9cff58ded
Dav <david.sanchez.martinez@outlook.com>
2026-07-16 16:43:29 +0200

    feat: create Quiz component

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,22 @@
+import Question from "./Question"
+
+const Quiz = ({ questions }) => {
+
+    const questionCards = questions.map((question, index) => {
+        return (
+            <Question
+                key={index}
+                question={question}
+            />
+        )
+    })
+
+    console.log(questionCards)
+    return (
+        <section className="quiz-section">
+            {questionCards}
+        </section>
+    )
+}
+
+export default Quiz
