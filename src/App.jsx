4506a211fc8ff6c302c02fa4ea87cc7492f3b360
Dav <david.sanchez.martinez@outlook.com>
2026-07-16 14:06:44 +0200

    feat: add quiz loading state and API integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,41 @@ import './App.css'
 import StartScreen from './components/StartScreen'
 
 function App() {
-  
-  const [screenStatus, setScreenStatus] = useState('intro')
-  const intro = screenStatus === 'intro'
 
+  const [screenStatus, setScreenStatus] = useState('intro')
+  const [questions, setQuestions] = useState([])
+
+  const intro = screenStatus === 'intro'
+  const loading = screenStatus === 'loading'
+  const playing = screenStatus === 'playing'
+
+  const fetchQuestions = async () => {
+      const questionsRes = await fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+      const data = await questionsRes.json()
+
+      setQuestions(data.results)
+      setScreenStatus('playing')
+  }
 
   const handleStartQuiz = () => {
-    setScreenStatus('playing')
+    setScreenStatus('loading')
+    fetchQuestions()
   }
+
+  console.log(questions)
 
   return (
     <main className='container'>
       {intro ?
-        <StartScreen 
+        <StartScreen
           onStartQuiz={handleStartQuiz}
-        /> : screenStatus
+        /> :
+        loading ?
+          <h1 className="start-screen-header container">
+            LOADING QUESTIONS...
+          </h1>
+          :
+          screenStatus
       }
     </main>
   )
