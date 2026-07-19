e54ecfb7920a022cbabb191a419af07fb17fe463
Dav <david.sanchez.martinez@outlook.com>
2026-07-19 17:03:08 +0200

    feat: render answer buttons in Question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,28 @@
+
 const Question = ({ question }) => {
 
+    const correctAnswer = question.correctAnswer
+    const answers = question.incorrectAnswers.concat(question.correctAnswer)
+
+    const answerButtons = answers.map((answer) => {
+        return (
+            <button className="answer-btn" key={answer}>
+                {answer}
+            </button>
+        )
+    })
+
+    console.log(answers)
+    console.log(correctAnswer)
+
     return (
-        <div className="question-div">
+        <div className="question-div container">
             <h2 className="question-header">
                 {question.question}
             </h2>
+            <div className="answers-div">
+                {answerButtons}
+            </div>
 
         </div>
     )
