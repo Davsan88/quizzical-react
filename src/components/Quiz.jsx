7292ee0c9a0f058c8182be579e9110a1e11384d5
Dav <david.sanchez.martinez@outlook.com>
2026-07-23 17:13:38 +0200

    feat:  create answer state management  and handler function to update them

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import Question from "./Question"
 
 const Quiz = ({ questions }) => {
 
-   
+    const [selectedAnswers, setSelectedAnswers] = useState([])
+
+    const handleSelectedAnswer = (answer, questionIndex) => {
+        setSelectedAnswers(prevAnswers => {
+            const updatedAnswers = [...prevAnswers]
+            updatedAnswers[questionIndex] = answer
+            return updatedAnswers
+    })
+    }
+
     const questionCards = questions.map((question, index) => {
         return (
             <Question
                 key={index}
+                questionIndex={index}
                 question={question}
+                selectedAnswers={selectedAnswers}
+                handleSelectedAnswer={handleSelectedAnswer}
             />
         )
     })
@@ -15,6 +28,9 @@ const Quiz = ({ questions }) => {
     return (
         <section className="quiz-section">
             {questionCards}
+            <button className="check-btn">
+                Check answers
+            </button>
         </section>
     )
 }
